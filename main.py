f730b8c942e33a7f2e0c9eee1d91c517446b630b
Ganesh <ganuofficial2795@gmail.com>
2021-05-15 13:22:52 +0530

    Notepad text

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pyautogui as gui
 import pytesseract as pyt
 from PIL import Image
+from time import sleep
 
 pyt.pytesseract.tesseract_cmd = r'C:\Users\GANESH\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
@@ -15,6 +16,15 @@ def Image_text():
     text = pyt.image_to_string(img)
     # print(text)
 
+    def Notepad_text(text):
+        '''Writing text on notepad'''
+
+        sleep(3)
+        gui.write(text,interval=0.25)
+
+    Notepad_text(text)
+
+    
 
 if __name__ == "__main__":
 
