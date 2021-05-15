0e002002fed7fa8dcffe11a392ea5c2d72284e98
Ganesh <ganuofficial2795@gmail.com>
2021-05-15 13:33:37 +0530

    Result Saved

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pyt.pytesseract.tesseract_cmd = r'C:\Users\GANESH\AppData\Local\Programs\Tessera
 def Image_text():
     '''Getting text from an image'''
 
-    img = Image.open(r'Images\testImage1.jpg')
+    img = Image.open(r'Images\testImage2.jpg')
     # img.show('images')
 
     text = pyt.image_to_string(img)
@@ -20,11 +20,21 @@ def Image_text():
         '''Writing text on notepad'''
 
         sleep(3)
-        gui.write(text,interval=0.25)
+        gui.write(text,interval = 0.15)
 
     Notepad_text(text)
 
-    
+    def Adding_path():
+        '''Saving the file'''
+
+        root = r"C:\Users\GANESH\OneDrive\Desktop\MyProjects\Image-Notes"
+        path = f"{root}\Results\data.txt"
+
+        gui.hotkey('ctrl','s')
+        gui.write(path)
+        gui.click(977,735)
+
+    Adding_path()
 
 if __name__ == "__main__":
 
